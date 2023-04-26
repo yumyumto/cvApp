47b0f41e0df3a5bc03cc0010a78fab2ff1dd4006
AmirShakouri <forworkandeffamir@gmial.com>
2023-04-26 21:09:34 +0430

    making some upgrade on app

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,10 @@ class Application extends StatelessWidget {
 
   PreferredSizeWidget _getAppBar() {
     return AppBar(
-      elevation: 2,
+      title: Text("امیر شکوری"),
+      elevation: 5,
       backgroundColor: Colors.black,
-      title: Center(
-        child: Text("امیر شکوری"),
-      ),
+      centerTitle: true,
     );
   }
 
@@ -45,36 +44,48 @@ class Application extends StatelessWidget {
   Widget _getHeader() {
     return Column(
       children: [
-        SizedBox(height: 16),
+        SizedBox(height: 18),
         Center(
           child: CircleAvatar(
             backgroundImage: AssetImage('images/user.png'),
             radius: 42,
           ),
         ),
-        SizedBox(height: 8),
+        SizedBox(height: 12),
         Text(
-          "من برنامه نویس و مدرس فلاتر هستم",
-          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w900),
+          "امیر شکوری هستم",
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        Text(
+          "برنامه نویس و مدرس فلاتر",
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
         ),
         SizedBox(height: 4),
-        Text(
-          "هرچیزی رو که یاد میگیریم رو دوست دارم با شما به اشتراک بذارم اگه علاقه مند به کار های من هستید میتونید یوتیوب من رو دنبال کنید",
-          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
-          textAlign: TextAlign.center,
+        Container(
+          padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+          child: Text(
+            "هرچیزی رو که یاد میگیرم رو با شما به اشتراک میذارم اگه علاقه مند به کار های من هستید میتونید یوتیوب من رو دنبال کنید",
+            style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+            textAlign: TextAlign.right,
+            textDirection: TextDirection.rtl,
+          ),
         ),
         _getRowIcons(),
         SizedBox(
           height: 12,
         ),
         _getSkillLables(),
+        SizedBox(width: 20),
+        _getResume(),
       ],
     );
   }
 
   Widget _getRowIcons() {
     return Wrap(
-      //! mainAxisAlignment: MainAxisAlignment.center,
       runSpacing: 32,
       spacing: 12,
       alignment: WrapAlignment.spaceEvenly,
@@ -122,6 +133,40 @@ class Application extends StatelessWidget {
             shadowColor: Colors.black,
           ),
       ],
+    );
+  }
+
+  Widget _getResume() {
+    final List<String> textLists = [
+      'برنامه نویس اندروید زرین پال از سال 97',
+      'هشت ترم ورکشاپ عملی اندروید دانشگاه تهران',
+      'چنل آموزشی یوتیوب از سال 93',
+      'مدرس دوره اندروید هاروارد (iran cs50x)',
+      'اموزش برنامه نویشی اندروید از سال 93',
+      'مدرس و بنیان گذار سایت ExpertFlutter'
+    ];
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      width: double.infinity,
+      color: Colors.grey[200],
+      child: Column(
+        children: [
+          Text(
+            "سابقه کاری من",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          for (var item in textLists)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(width: double.infinity),
+                Text(item, textDirection: TextDirection.rtl),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
